f10f0be0003e9238c586a2cede77a5ce22178a6a
Daniel Hutchings <danielhutchings@Harambe-2.local>
2020-12-05 11:30:01 -0700

    First MLP

--- our_mlp.py
+++ our_mlp.py
@@ -1,10 +1,15 @@
 import dataextractor as de
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
 
 extr = de.DataExtractor()
-arr = extr.load_json().to_array()
+data = extr.load_json().to_array()
 
-print(len(arr))
-print(len(arr[0]))
+print(len(data))
+print(len(data[0]))
 
 # Acousticiness [0]
 # Dancibility [1]
@@ -16,8 +21,29 @@ print(len(arr[0]))
 # Liveness [7]
 # Loudness [8]
 # Mode [9]
-# Popularity [10]
+#d
 # Speechiness [11]
 # Tempo [12]
 # Time signature [13]
 # Valence [14]
+
+print(data[0])
+labels = data[:,-1]
+data = data[:,:-1]
+data = extr.normalize(data)
+print(data[0])
+print(labels[0])
+
+X_train, X_test, y_train, y_test = train_test_split(data, labels, random_state=1, test_size=.3)
+print(len(X_train))
+print(len(X_test))
+
+clf = MLPRegressor().fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
+
+X_train, X_test, y_train, y_test = train_test_split(data, labels,
+                                                    random_state=1)
+regr = MLPRegressor(random_state=1, max_iter=500).fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
