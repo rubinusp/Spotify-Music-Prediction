4a12f685fc9d4e2de19580569c1e8834d71d69cc
Daniel Hutchings <danielhutchings@Harambe-2.local>
2020-12-07 21:16:48 -0700

    Yup

--- classifier.py
+++ classifier.py
@@ -17,9 +17,12 @@ labels = data[:,-1]
 data = data[:,:-1]
 data = extr.normalize(data)
 
-per = .7
-new_labels = extr.catagorize(labels, per)
-X_train, X_test, y_train, y_test = train_test_split(data, new_labels, stratify=new_labels)
-clf = MLPClassifier(nesterovs_momentum=True, max_iter=500).fit(X_train, y_train)
-accuracy = clf.score(X_test, y_test)
-print(accuracy)
+percentages = [.1, .2, .3, .4, .5, .6, .7, .8, .9]
+# Change this: .7 means 70% are not popular
+
+for per in percentages:
+    new_labels = extr.catagorize(labels, per)
+    X_train, X_test, y_train, y_test = train_test_split(data, new_labels, stratify=new_labels)
+    clf = MLPClassifier(nesterovs_momentum=True, max_iter=500).fit(X_train, y_train)
+    accuracy = clf.score(X_test, y_test)
+    print(accuracy)
